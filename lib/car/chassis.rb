9df8b9ac6e1eab7f127af1e3bed46d4114c7b294
Tomato <Tomato.HF@gmail.com>
2015-05-10 22:46:39 +0800

    Car::Chassis

---
+++ lib/car/chassis.rb
@@ -0,0 +1,26 @@
+module Car
+  class Chassis
+    def initialize(wheel_lf, wheel_rf, wheel_lr, wheel_rr)
+      @wheel_left_front = wheel_lf
+      @wheel_right_front = wheel_rf
+      @wheel_left_rear = wheel_lr
+      @wheel_right_rear = wheel_rr
+    end
+
+    def stop
+      wheels.each { |wheel| wheel.stop }
+    end
+
+    def left_wheels
+      [@wheel_left_front, @wheel_left_rear]
+    end
+
+    def right_wheels
+      [@wheel_right_front, @wheel_right_rear]
+    end
+
+    def wheels
+      left_wheels + right_wheels
+    end
+  end
+end
