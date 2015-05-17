075434ddfbf90462dad8c8b0b184d9246e12de07
Tomato <Tomato.HF@gmail.com>
2015-05-17 19:46:52 +0800

    control wheel speed

--- lib/car/wheel.rb
+++ lib/car/wheel.rb
@@ -5,19 +5,25 @@ module Car
     def initialize(pin1, pin2, pwm, frequency)
       @pin1 = pin1
       @pin2 = pin2
+      @pwm = pwm
+      @range = (1000000 / 100 / frequency).round
 
       @gpio = WiringPi::GPIO.new
       each_pin { |pin| @gpio.pin_mode(pin, WiringPi::OUTPUT) }
+      @gpio.soft_pwm_create(@pwm, 0, @range)
     end
 
     def stop
+      @gpio.soft_pwm_write(@pwm, 0)
       each_pin { |pin| @gpio.digital_write(pin, WiringPi::LOW) }
     end
 
-    def direction=(value)
+    def speed=(value)
       if (value == 0)
         stop
       else
+        @gpio.soft_pwm_write(@pwm, [value.abs, 1].min * @range)
+
         pin_high, pin_low = (value > 0) ? pins : pins.reverse
         @gpio.digital_write(pin_high, WiringPi::HIGH)
         @gpio.digital_write(pin_low, WiringPi::LOW)
